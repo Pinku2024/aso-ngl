28f8cd7a0b0c67e229432d79b3f390a799ab5d23
Pinku2024 <pinku@multivariate.tech>
2024-05-27 17:48:55 +0530

    👍

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -93,10 +93,19 @@ const ContactForm = () => {
                     className="image request-a-quote-contact"
                   />
                 </div>
-                <div className="button-primary" style={{height:"14px", lineHeight:"0"}}>To increase tap potential</div>
-                {/* <div>
-                  <a className="button-primary w-button" style={{height: "18px", marginBottom: "18px"}} href="mailto:contact@nextgrowthlabs.com">To increase tap potential</a>
-                  </div> */}
+                <div
+                  className="button-primary"
+                  style={{
+                    height: "14px",
+                    lineHeight: "0",
+                    ...(typeof window !== "undefined" &&
+                      window.innerWidth <= 375 && {
+                        fontSize: "14px",
+                      }),
+                  }}
+                >
+                  To increase tap potential
+                </div>
               </Link>
               <Link
                 href="tel:(546)232-9872"
