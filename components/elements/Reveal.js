d9f43d481da5e774ba481577f1f984a1e5a62635
Pinku2024 <pinku@multivariate.tech>
2024-06-08 19:04:29 +0530

    console warnings

--- components/elements/Reveal.js
+++ components/elements/Reveal.js
@@ -1,9 +1,11 @@
 import { useRef, useEffect } from "react";
 import { motion, useAnimation, useInView } from "framer-motion";
+
 const Reveal = ({ children }) => {
   const ref = useRef();
   const mainControl = useAnimation();
   const isInView = useInView(ref, { once: false });
+
   useEffect(() => {
     if (isInView) {
       mainControl.start("visible");
@@ -11,30 +13,17 @@ const Reveal = ({ children }) => {
       mainControl.start("hidden");
     }
   }, [isInView]);
+
   return (
     <motion.div
       ref={ref}
       variants={{
         hidden: {
-          WebkitTransform:
-            "translate3d(0, 60px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
-          MozTransform:
-            "translate3d(0, 60px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
-          msTransform:
-            "translate3d(0, 60px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
-          transform:
-            "translate3d(0, 60px, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
+          translateY: 60,
           opacity: 0,
         },
         visible: {
-          WebkitTransform:
-          "translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
-          MozTransform:
-          "translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
-          msTransform:
-          "translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
-          transform:
-          "translate3d(0, 0, 0) scale3d(1, 1, 1) rotateX(0) rotateY(0) rotateZ(0) skew(0, 0)",
+          translateY: 0,
           opacity: 1,
         },
       }}
